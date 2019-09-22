b84073d4208620e06776a4da1e079d14cca93504
West14 <30056636+West14@users.noreply.github.com>
2019-09-22 22:57:16 +0300

    1.0.0

---
+++ Listener.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andriy
+ * Date: 12.06.2019
+ * Time: 22:37
+ * Made with <3 by West from TechGate Studio
+ */
+
+namespace West\ProtectACP;
+
+
+class Listener
+{
+    public static function postDispatchAdminLogin(\XF\Mvc\Controller $controller, $action, \XF\Mvc\ParameterBag $params, \XF\Mvc\Reply\AbstractReply &$reply)
+    {
+        if (strtolower($action) == 'form')
+        {
+            $cookie = $controller->request()->getCookie('user');
+            if ($cookie)
+            {
+                $userId = stristr($cookie, ',', true);
+                /** @var \XF\Entity\User $user */
+                $user = \XF::em()->find('XF:User', $userId);
+                if (!$user->is_admin) $reply = $controller->redirect(\XF::app()->router('public')->buildLink('index'));
+            }
+            else
+            {
+                $reply = $controller->redirect(\XF::app()->router('public')->buildLink('index'));
+            }
+        }
+    }
+}
